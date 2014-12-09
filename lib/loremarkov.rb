ddc08b9777f19675124eb60a3e4db314122e8d84
Rick Hull <rick.hull@gmail.com>
2014-12-08 21:15:07 -0500

    fix up comments; make rocco swoon

--- lib/loremarkov.rb
+++ lib/loremarkov.rb
@@ -1,20 +1,55 @@
+# **Loremarkov** uses Markov chains to generate plausible-sounding text, given
+# an input corpus.  It comes with a few built-in sample texts.
+#
+# It is based upon Kernighan & Pike's *The Practice of Programming* Chapter 3
+#
+# Install Loremarkov with Rubygems:
+#
+#     gem install loremarkov
+#
+# Once installed, the `destroy` command can be used to generate plausible-
+# sounding text.  The input text may be provided by filename, STDIN, or naming
+# one of the built-in sample texts.
+#
+#     destroy lorem_ipsum
+#     destroy ~/my_first_corpus.txt
+#     man ls | destroy
+
 class Loremarkov
+  ##### TOKENS - These tokens are what splits the text into words.
+  # In contrast to ruby's String#split, these tokens are included in the
+  # resulting array.
   TOKENS = ["\n", "\t", ' ', "'", '"']
 
-  # Decompose text into an array of tokens, including and delimited by TOKENS
-  # e.g. "Hello", he said.
-  # # => ['"', 'Hello', '"', ',', ' ', 'he', ' ', 'said.',]
+  ##### lex - Decompose text into an array of tokens and words
+  # Words are merely the string of characters between the nearest two TOKENS
+  # e.g.
+  #
+  #     lex %q{"Hello", he said.}
+  #
+  # becomes
+  #
+  #     - %q{"}      # TOKEN
+  #     - %q{Hello}  # word
+  #     - %q{"}      # TOKEN
+  #     - %q{,}      # word
+  #     - %q{ }      # TOKEN
+  #     - %q{he}     # word
+  #     - %q{ }      # TOKEN
+  #     - %q{said.}  # word
+  #
   # This operation can be losslessly reversed by calling #join on the resulting
   # array.
-  # i.e. lex(str).join == str
+  # i.e. `lex(str).join == str`
   #
   def self.lex(str, tokens = TOKENS)
     final_ary = []
     word = ''
-    str.each_byte { |b|   # yes I am terrible with encodings
-      # either a token (thereby ending the current word)
-      # or part of the current word
-      #
+    # This code makes no attempt to deal with non-ASCII string encodings.
+    # i.e.  byte-per-char
+    str.each_byte { |b|
+      # This byte is either a token, thereby ending the current word
+      # or it is part of the current word
       if tokens.include?(b.chr)
         final_ary << word if !word.empty?
         final_ary << b.chr
@@ -28,63 +63,56 @@ class Loremarkov
   end
 
 
-  # Generate a markov data structure
-  # Arrays of string for keys and values
-  # Keys are prefixes -- ordered word sequence of constant length
-  # Values are an accumulation of the next word after the prefix, however many
-  # times it may occur.
-  # e.g. If a prefix occurs twice, then the value will be
-  # an array of two words -- possibly the same word twice.
-  #
+  ##### analyze - Generate a markov data structure
+  # * Arrays of string for keys and values
+  # * Keys are prefixes -- ordered word sequence of constant length
+  # * Values are an accumulation of the next word after the prefix, however
+  #   many times it may occur.
+  # * e.g. If a prefix occurs twice, then the value will be an array of two
+  #   words -- possibly the same word twice.
   def self.analyze(text, num_prefix_words)
     markov = {}
     words = lex(text)
 
-    # Go through the possible valid prefixes.
-    # Adding 1 gives you the final key:
-    # *num_prefix_words* words with a nil value  -- signifying EOF
-    #
+    # Go through the possible valid prefixes.  Adding 1 gives you the final
+    # key: *num_prefix_words* words with a nil value  -- signifying EOF
     (words.length - num_prefix_words + 1).times { |i|
       prefix_words = []
       num_prefix_words.times { |j| prefix_words << words[i + j] }
-
-      # set to empty array on a new prefix
-      #
+      # Set to empty array on a new prefix.
+      # Add the target word, which will be nil on the last iteration
       markov[prefix_words] ||= []
-      # add the target word, which will be nil on the last iteration
       markov[prefix_words] << words[i + num_prefix_words]
     }
     markov
   end
 
-  # given the entire text, use an extremely conservative heuristic
-  # to grab only the first chunk to pass to lex
-  #
+  # Given the entire text, use an extremely conservative heuristic to grab only
+  # the first chunk to pass to lex
   def self.start_prefix(text, num_prefix_words)
     lex(text[0, 999 * num_prefix_words])[0, num_prefix_words]
   end
 
   attr_reader :markov
 
+  # More prefix_words means tighter alignment to original text
   def initialize(num_prefix_words)
     @num_prefix_words = num_prefix_words
     @markov = {}
   end
 
-  # text should have a definite end, not just a convenient buffer split
-  #
+  # Generate Markov structure from text.
+  # Text should have a definite end, not just a convenient buffer split
   def analyze(text)
     @markov.merge!(self.class.analyze(text, @num_prefix_words))
   end
 
-  # given a prefix, give me the next word
-  #
+  # Generate the next word for a given prefix
   def generate_one(prefix_words)
     @markov.fetch(prefix_words).sample
   end
 
-  # given the start prefix, generate words until EOF
-  #
+  # Given the start prefix, generate words until EOF
   def generate_all(start_prefix_words)
     words = start_prefix_words
     while tmp = generate_one(words[-1 * @num_prefix_words, @num_prefix_words])
@@ -93,8 +121,7 @@ class Loremarkov
     words.join
   end
 
-  # do it, you know you want to
-  #
+  # Do it, you know you want to
   def destroy(text)
     analyze(text)
     generate_all(self.class.start_prefix(text, @num_prefix_words))

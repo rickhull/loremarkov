e489ddec0ed2888709c4e0b3c7a336fe0199962c
Rick Hull <rick.hull@gmail.com>
2022-05-01 18:08:33 -0400

    try nix flakes

---
+++ flake.nix
@@ -0,0 +1,41 @@
+# HOW TO USE:
+# 1. make a `profiles/` directory in your repo and add to .gitignore
+# 2. nix develop --profile profiles/dev
+# 3. done!
+#
+# the shell is completely safe from garbage collection and evaluates instantly
+# due to Nix's native caching. if you want logs during build, add `-L` to 
+# `nix develop`.
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }: let
+    inputs = { inherit nixpkgs; };
+    system = "x86_64-linux";
+
+    pkgs = nixpkgs.legacyPackages."${system}";
+
+    # or, if you need to add an overlay:
+    # pkgs = import nixpkgs {
+    #   inherit system;
+    #   overlays = [
+    #     (import ./nix/overlay.nix)
+    #   ];
+    # };
+
+    # a text file containing the paths to the flake inputs in order to stop
+    # them from being garbage collected
+    pleaseKeepMyInputs = pkgs.writeTextDir "bin/.please-keep-my-inputs"
+      (builtins.concatStringsSep " " (builtins.attrValues inputs));
+  in {
+    devShell."${system}" = pkgs.mkShell {
+      buildInputs = [
+        (pkgs.ruby.withPackages (p: with p; [
+	  rake
+	]))
+
+        pleaseKeepMyInputs
+      ];
+    };
+  };
+}

0248c0760b04e05c1ed1b895ee4ae4db44e1cc3c
Rick Hull <rick.hull@gmail.com>
2022-05-02 18:42:09 -0400

    build nix flake for all default systems

--- flake.nix
+++ flake.nix
@@ -1,37 +1,20 @@
-# the shell is completely safe from garbage collection and evaluates instantly
-# due to Nix's native caching. if you want logs during build, add `-L` to
-# `nix develop`.
 {
+  description = "Provides a shell with useful gems activated";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: let
-    inputs = { inherit nixpkgs; };
-    system = "x86_64-linux";
-
-    pkgs = nixpkgs.legacyPackages."${system}";
-
-    # or, if you need to add an overlay:
-    # pkgs = import nixpkgs {
-    #   inherit system;
-    #   overlays = [
-    #     (import ./nix/overlay.nix)
-    #   ];
-    # };
-
-    # a text file containing the paths to the flake inputs in order to stop
-    # them from being garbage collected
-    flakeRoot = pkgs.writeTextDir "bin/.flakeRoot"
-      (builtins.concatStringsSep " " (builtins.attrValues inputs));
-  in {
-    devShell."${system}" = pkgs.mkShell {
-      buildInputs = [
-        (pkgs.ruby.withPackages (p: with p; [
-	  rake
-	  minitest
-	]))
-
-        flakeRoot
-      ];
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = [
+          (pkgs.ruby.withPackages (p: with p; [
+            rake
+            minitest
+          ]))
+        ];
+      };
+    });
 }
